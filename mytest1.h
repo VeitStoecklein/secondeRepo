a62d1e04180185a62bef0e11ab9345f886e8c0f7
Veit Stöcklein <veit.stoecklein@gmx.de>
2019-04-26 22:47:11 +0200

    Hier wird ein konflikt enstehen

--- mytest1.h
+++ mytest1.h
@@ -3,5 +3,6 @@
 main(){
 	printf("Hello World");
 	printf("Ebenfalls eine Zeile hinzugefügt während aktiv im Develop branch");
+	printf("Noch eine Zeile im Develop aber noch nicht gepusht");
 	return 0;
 }
