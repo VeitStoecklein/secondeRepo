60b667b8a856b7268ec3293a7b3fa81ff969b4d8
Veit Stoecklein <veit.stoecklein@bertrandt.com>
2021-11-29 15:55:10 +0100

    ADD local Line at laptop

--- mytest1.h
+++ mytest1.h
@@ -4,5 +4,6 @@ main(){
 	printf("Hello World");
 	printf("Ebenfalls eine Zeile hinzugefügt während aktiv im Develop branch");
 	printf("Noch eine Zeile im Develop aber noch nicht gepusht");
+	printf("Weitere Zeile auf lokalem repo hinzugefügt");
 	return 0;
 }
